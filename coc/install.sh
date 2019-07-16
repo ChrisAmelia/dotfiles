8442f1ea9dc24e29cdd6ea6f033f65246040bf57
ChrisAmelia <chris@objectdev.io>
2019-07-16 18:11:38 +0200

    Add symlink for coc in install.sh

--- coc/install.sh
+++ coc/install.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Create symlink for coc-settings.json
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+NVIM_DIRECTORY=$HOME/.config/nvim
+
+echo "Creating nvim's config directory if does not exist."
+mkdir -p $NVIM_DIRECTORY
+
+configs=(
+    coc-settings.json
+)
+
+echo "Creating symlinks"
+for i in "${configs[@]}"; do
+    ln -s $CURRENT_DIR/$i $NVIM_DIRECTORY
+done
+
+
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
