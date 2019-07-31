97e2f1594b4b0324666a380975815c554be28b40
ChrisAmelia <chris@objectdev.io>
2019-07-31 17:08:37 +0200

    Factorise zsh/install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -11,34 +11,59 @@ LIGHT_PURPLE='\033[1;35m'
 NC='\033[0m'
 MSG_AREADY_EXIST='ALREADY EXISTS'
 
+# FUNCTIONS
+echoMessage() {
+    echo -e "${YELLOW}$1${NC}"
+}
 
-# INSTALLING OH-MY-ZSH
-echo -e "${YELLOW}Downloading and installing Oh-My-Zsh...${NC}"
-INSTALL_OHMYZSH='sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
-printf "${LIGHT_CYAN}+$INSTALL_OHMYZSH${NC}\n"
-eval $INSTALL_OHMYZSH
+echoCommand() {
+    commands=()
+    for var in "$@"
+    do
+        commands+=$var" "
+    done
+    echo -e "${LIGHT_CYAN}+$commands${NC}"
+}
+
+echoSuccessFail() {
+    if [ $? -eq 0 ]; then
+        echo -e "... ${LIGHT_GREEN}OK${NC}"
+    else
+        echo -e "... ${LIGHT_RED}FAIL${NC}"
+    fi
+}
+
+<< "PARAMETERS"
+    $1: string message
+    $2: command to evaluate
+PARAMETERS
+evaluateCommand() {
+    echoMessage "$1"
+    echoCommand "$2"
+    eval "$2" > /dev/null
+    echoSuccessFail
+}
+
+installOhMyZsh() {
+    MESSAGE="Downloading and installing Oh-My-Zsh..."
+    COMMAND='sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+
+    evaluateCommand "$MESSAGE" "$COMMAND"
+}
 
 
-# SYMLINK
-configs=(
-    .zshrc
-)
-# Removing Symlink
-REMOVE_SYMLINK="rm -rf $HOME/.zshrc"
-echo ""
-echo -e "${YELLOW}Removing symlink$ zshrc${NC}"
-echo -e "${LIGHT_CYAN}$REMOVE_SYMLINK${NC}"
-eval $REMOVE_SYMLINK
-# Creating symlink
-echo ""
-echo -e "${YELLOW}Creating symlink in $HOME${NC}"
-for i in "${configs[@]}"; do
-    ln -s $CURRENT_DIR/$i $HOME
-done
-if [ $? -eq 0 ]; then
-    echo -e "... ${LIGHT_GREEN}OK${NC}"
-else
-    echo -e "... ${LIGHT_RED}FAIL${NC}"
-fi
+createSymlink() {
+    MESSAGE_REMOVE_SYMLINK="Removing current .zshrc"
+    COMMAND_REMOVE_SYMLINK="rm -rf $HOME/.zshrc"
+    evaluateCommand "$MESSAGE_REMOVE_SYMLINK" "$COMMAND_REMOVE_SYMLINK"
+
+
+    MESSAGE_CREATE_SYMLINK="Creating symlink in $HOME"
+    COMMAND_CREATE_SYMLINK="ln -s $CURRENT_DIR/.zshrc $HOME"
+    evaluateCommand "$MESSAGE_CREATE_SYMLINK" "$COMMAND_CREATE_SYMLINK"
+}
+
+installOhMyZsh
+createSymlink
 
 exit 0
