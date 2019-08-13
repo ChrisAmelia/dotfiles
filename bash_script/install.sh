51e11b1b7e5fe5d7da9e1165c2da4f6d91f25e48
ChrisAmelia <chris@objectdev.io>
2019-08-13 18:12:15 +0200

    bash: document install script

--- bash_script/install.sh
+++ bash_script/install.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-# COLORS
+###################################################################
+# @constans
+# @description:     color constant
+###################################################################
 LIGHT_RED='\033[1;31m'
 YELLOW='\033[1;33m'
 LIGHT_CYAN='\033[1;36m'
 LIGHT_GREEN='\033[1;32m'
 LIGHT_PURPLE='\033[1;35m'
 NC='\033[0m'
+MSG_AREADY_EXIST='ALREADY EXISTS'
 
+###################################################################
+# @constant
+# @description:     directories: current and local binaries
+###################################################################
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-LOCAL_BIN_DIR="/usr/local/bin"
+LOCAL_BIN="/usr/local/bin"
 
-# FUNCTIONS
+###################################################################
+# @description      Prints given message in yellow
+# @args             $1: message to print
+###################################################################
 echoMessage() {
     echo -e "${YELLOW}$1${NC}"
 }
 
-echoCommand() {
-    commands=()
-    for var in "$@"
-    do
-        commands+=$var" "
-    done
-    echo -e "${LIGHT_CYAN}+$commands${NC}"
-}
-
-echoSuccessFail() {
-    if [ $? -eq 0 ]; then
-        echo -e "... ${LIGHT_GREEN}OK${NC}"
-    else
-        echo -e "... ${LIGHT_RED}FAIL${NC}"
-    fi
-}
-
+###################################################################
+# @description      Create symlinks for bash scripts
+# @noargs
+###################################################################
 createSymLinks() {
-    CONFIGS=(
+    BASH_SCRIPTS=(
         update
         upgrade
         ccl
     )
 
     echoMessage "Creating symlinks"
-    for i in "${CONFIGS[@]}"; do
+    for i in "${BASH_SCRIPTS[@]}"; do
         sudo ln -s $CURRENT_DIR/$i.sh $LOCAL_BIN_DIR/$i
     done
 }
