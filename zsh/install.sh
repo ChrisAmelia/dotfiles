b1bcb8160e93d33dcf16687b141f5f92c0a38721
ChrisAmelia <chris@objectdev.io>
2019-08-13 18:23:54 +0200

    zsh: document intall script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+###################################################################
+# @constant
+# @description     current directory
+###################################################################
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# COLORS
+###################################################################
+# @constans
+# @description:     color constant
+###################################################################
 LIGHT_RED='\033[1;31m'
 YELLOW='\033[1;33m'
 LIGHT_CYAN='\033[1;36m'
@@ -11,11 +18,18 @@ LIGHT_PURPLE='\033[1;35m'
 NC='\033[0m'
 MSG_AREADY_EXIST='ALREADY EXISTS'
 
-# FUNCTIONS
+###################################################################
+# @description      Prints given message in yellow
+# @args             $1: message to print
+###################################################################
 echoMessage() {
     echo -e "${YELLOW}$1${NC}"
 }
 
+###################################################################
+# @description      Prints given message in cyan
+# @args             $1: command to print
+###################################################################
 echoCommand() {
     commands=()
     for var in "$@"
@@ -25,6 +39,10 @@ echoCommand() {
     echo -e "${LIGHT_CYAN}+$commands${NC}"
 }
 
+###################################################################
+# @description      Echo success of last last executed command
+# @noargs
+###################################################################
 echoSuccessFail() {
     if [ $? -eq 0 ]; then
         echo -e "... ${LIGHT_GREEN}OK${NC}"
@@ -33,20 +51,11 @@ echoSuccessFail() {
     fi
 }
 
-echoSuccessFailGitClone() {
-    if [ $1 -eq 0 ]; then
-        echo -e "${LIGHT_GREEN}OK${NC}"
-    elif [ $1 -eq 128 ]; then
-        echo -e "${LIGHT_GREEN}ALREADY PRESENT${NC}"
-    else
-        echo -e "${LIGHT_RED}FAIL${NC}"
-    fi
-}
-
-<< "PARAMETERS"
-    $1: string message
-    $2: command to evaluate
-PARAMETERS
+###################################################################
+# @description      Print command and evalute it
+# @args             $1: message to print
+#                   $2: command to execute
+###################################################################
 evaluateCommand() {
     echoMessage "$1"
     echoCommand "$2"
@@ -54,6 +63,10 @@ evaluateCommand() {
     echoSuccessFail
 }
 
+###################################################################
+# @description      Install oh-my-zsh
+# @noargs
+###################################################################
 installOhMyZsh() {
     MESSAGE="Downloading and installing Oh-My-Zsh..."
     COMMAND='sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
@@ -61,6 +74,10 @@ installOhMyZsh() {
     evaluateCommand "$MESSAGE" "$COMMAND"
 }
 
+###################################################################
+# @description      Remove current .zshrc and create symlink
+# @noargs
+###################################################################
 createSymlink() {
     MESSAGE_REMOVE_SYMLINK="Removing current .zshrc"
     COMMAND_REMOVE_SYMLINK="rm -rf $HOME/.zshrc"
@@ -72,24 +89,26 @@ createSymlink() {
     evaluateCommand "$MESSAGE_CREATE_SYMLINK" "$COMMAND_CREATE_SYMLINK"
 }
 
+###################################################################
+# @description      Install zsh-syntax-highlighting
+# @noargs
+###################################################################
 installZshSyntaxHighlighting() {
     MESSAGE="Installing zsh-syntax-highlighting..."
     COMMAND="git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-    echoMessage "$MESSAGE"
-    echoCommand "$COMMAND"
-    eval "$COMMAND" > /dev/null 2>&1
-    echoSuccessFailGitClone $?
+    evaluateCommand "$MESSAGE" "$COMMAND"
 }
 
+###################################################################
+# @description      Install zsh-autosuggestions
+#noargs
+###################################################################
 installZshAutosuggestions() {
     MESSAGE="Installing zsh-autosuggestions..."
     COMMAND="git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-    echoMessage "$MESSAGE"
-    echoCommand "$COMMAND"
-    eval "$COMMAND" > /dev/null 2>&1
-    echoSuccessFailGitClone $?
+    evaluateCommand "$MESSAGE" "$COMMAND"
 }
 
 installOhMyZsh
