93ff004ac16e9e8b16eced1bc6ca3efad9139cf2
ChrisAmelia <chris@objectdev.io>
2019-08-20 10:22:11 +0200

    packages: documentation for install script

--- packages/install.sh
+++ packages/install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #------------------------------------------------------------------
-# @constans
-# @description:     color constant
+# @description      Colors constants
 #------------------------------------------------------------------
-LIGHT_RED='\033[1;31m'
-YELLOW='\033[1;33m'
-LIGHT_CYAN='\033[1;36m'
-LIGHT_GREEN='\033[1;32m'
-LIGHT_PURPLE='\033[1;35m'
-NC='\033[0m'
+readonly LIGHT_RED='\033[1;31m'
+readonly YELLOW='\033[1;33m'
+readonly LIGHT_CYAN='\033[1;36m'
+readonly LIGHT_GREEN='\033[1;32m'
+readonly LIGHT_PURPLE='\033[1;35m'
+readonly NC='\033[0m'
 
 #------------------------------------------------------------------
 # @description      Prints given message in yellow
-# @args             $1: message to print
+# @arg              $1: message to print
 #------------------------------------------------------------------
 echoMessage() {
     echo -e "${YELLOW}$1${NC}"
@@ -21,7 +20,7 @@ echoMessage() {
 
 #------------------------------------------------------------------
 # @description      Prints given message in cyan
-# @args             $1: command to print
+# @arg              $1: command to print
 #------------------------------------------------------------------
 echoCommand() {
     commands=()
@@ -34,7 +33,6 @@ echoCommand() {
 
 #------------------------------------------------------------------
 # @description      Echo success of last last executed command
-# @noargs
 #------------------------------------------------------------------
 echoSuccessFail() {
     if [ $? -eq 0 ]; then
@@ -46,8 +44,8 @@ echoSuccessFail() {
 
 #------------------------------------------------------------------
 # @description      Print command and evalute it
-# @args             $1: message to print
-#                   $2: command to execute
+# @arg              $1: message to print
+# @arg              $2: command to execute
 #------------------------------------------------------------------
 evaluateCommand() {
     echoMessage "$1"
@@ -58,10 +56,9 @@ evaluateCommand() {
 
 #------------------------------------------------------------------
 # @description      Look for package in 'list_packages' and install them
-# @noargs
 #------------------------------------------------------------------
 installPackages() {
-    PACKAGES_FILE="list_packages"
+    readonly PACKAGES_FILE="list_packages"
     packages=()
 
     while IFS= read -r package
@@ -69,32 +66,30 @@ installPackages() {
         packages+=$package" "
     done < "$PACKAGES_FILE"
 
-    MESSAGE="Installing following packages: $packages"
-    COMMAND="sudo apt install $packages"
+    readonly MESSAGE_INSTALL_PACKAGE="Installing following packages: $packages"
+    readonly COMMAND_INSTALL_PACKAGE="sudo apt install $packages"
 
-    evaluateCommand "$MESSAGE" "$COMMAND"
+    evaluateCommand "$MESSAGE_INSTALL_PACKAGE" "$COMMAND_INSTALL_PACKAGE"
 }
 
 #------------------------------------------------------------------
 # @description      Update npm version
-# @noargs
 #------------------------------------------------------------------
 updateNpm() {
-    MESSAGE="Updating npm"
-    COMMAND="npm install -g npm"
+    readonly MESSAGE_UPDATE_NPM="Updating npm"
+    readonly COMMAND_UPDATE_NPM="npm install -g npm"
 
-    evaluateCommand "$MESSAGE" "$COMMAND"
+    evaluateCommand "$MESSAGE_UPDATE_NPM" "$COMMAND_UPDATE_NPM"
 }
 
 #------------------------------------------------------------------
 # @description      Install Python3 for Neovim
-# @noargs
 #------------------------------------------------------------------
 installPythonForNeovim() {
-    MESSAGE="Installing Python3 for Neovim"
-    COMMAND="pip3 install neovim"
+    readonly MESSAGE_INSTALL_PYTHON3_NVIM="Installing Python3 for Neovim"
+    readonly COMMAND_INSTALL_PYTHON3_NVIM="pip3 install neovim"
 
-    evaluateCommand "$MESSAGE" "$COMMAND"
+    evaluateCommand "$MESSAGE_INSTALL_PYTHON3_NVIM" "$COMMAND_INSTALL_PYTHON3_NVIM"
 }
 
 installPackages
