f1e2c274bd65088847c7916d16f93451316c5371
ChrisAmelia <chris@objectdev.io>
2020-12-03 23:17:50 +0100

    refactor(settings)

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,93 +1,54 @@
--- Show line numbers on the sidebar
-vim.wo.number = true
+local api           = vim.api
+local cmd           = vim.cmd
+local option        = vim.o
+local window_option = vim.wo
 
--- Show the line number relative to the line with the cursor in front of each line
-vim.wo.relativenumber = true
+option.autoindent  = true    -- Autoindent when starting new line, or using o or O
+option.background  = 'dark'  -- When set to "dark" or "light", adjusts the default color groups for that background type.
+option.encoding    = 'utf-8' -- Force utf-8 encoding
+option.hlsearch    = true    -- Enable search highlighting.
+option.ignorecase  = true    -- Ignore case when searching
+option.incsearch   = true    -- Enable highlighted case-insensitive incremential search.
+option.modeline    = false   -- Don't parse modelines (google vim modeline vulnerability).
+option.modifiable  = true    -- Make buffer modifiable
+option.shiftwidth  = 4       -- When indenting with '>', use spaces width
+option.showcmd     = true    -- Show the size of block one selected in visual mode
+option.showmatch   = true    -- The cursor will briefly jump to the matching brace when you insert one.
+option.splitright  = true    -- When on, splitting a window will put the new window right of the current one.
+option.tabstop     = 4       -- Show existing tab with spaces width
+option.ttimeoutlen = 50      -- Allow for mappings including Esc, while preserving zero timeout after pressing it manually
+option.updatetime  = 100     -- Update time in ms
+option.wildoptions = 'pum'   -- List of words that change how |cmdline-completion| is done.
+option.clipboard   = "unnamedplus"               -- Register clipboard
+option.completeopt = "menuone,noinsert,noselect" -- A comma separated list of options for Insert mode completion |ins-completion|
+-- option.wildmenu = true -- Autocomplete commands using nice menu in place of window status. Enable CTRL-N and CTRL-P to scroll through matches.
+-- option.wildmode = 'longest:full,full'-- For autocompletion, complete as much as you can.
 
--- Ignore case when searching
-vim.o.ignorecase = true
 
--- Highlight line under cursor. It helps with navigation.
-vim.wo.cursorline = true
+window_option.cursorline     = true -- Highlight line under cursor. It helps with navigation.
+window_option.number         = true -- Show line numbers on the sidebar
+window_option.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line
 
--- Force utf-8 encoding
-vim.o.encoding = 'utf-8'
 
--- Allow for mappings including Esc, while preserving zero timeout after pressing it manually
-vim.o.ttimeoutlen = 50
+cmd("colorscheme sublimemonokai") -- Set colorscheme
+cmd("set noswapfile")             -- Disable swap to prevent annoying messages.
+cmd("syntax on")                  -- See :h :syntax-on
 
--- Show the size of block one selected in visual mode
-vim.o.showcmd = true
 
--- Autocomplete commands using nice menu in place of window status. Enable CTRL-N and CTRL-P to scroll through matches.
--- vim.o.wildmenu = true
+vim.g.mapleader = "," -- Leader map
 
--- For autocompletion, complete as much as you can.
--- vim.o.wildmode = 'longest:full,full'
 
--- The cursor will briefly jump to the matching brace when you insert one.
-vim.o.showmatch = true
+api.nvim_command("hi Comment     guibg=none guifg=#7CFC00" ) -- Comments highlight
+api.nvim_command("hi LineNr      guibg=none guifg=none"    ) -- Number column highlight
+api.nvim_command("hi Normal      guibg=NONE"               ) -- Transparent background
+api.nvim_command("hi NormalFloat guibg=#2F4F4F"            ) -- Floatin window highlight
+api.nvim_command("hi SignColumn  guibg=none"               ) -- Sign column highlight
+api.nvim_command("hi Visual      guibg=#00FA9A guifg=BLACK") -- Visual highlight
 
--- Enable search highlighting.
-vim.o.hlsearch = true
-
--- Enable highlighted case-insensitive incremential search.
-vim.o.incsearch = true
-
--- Autoindent when starting new line, or using o or O
-vim.o.autoindent = true
-
--- Don't parse modelines (google vim modeline vulnerability).
-vim.o.modeline = false
-
--- Disable swap to prevent annoying messages.
-vim.cmd("set noswapfile")
-
--- Make buffer modifiable
-vim.o.modifiable = true
-
--- Show existing tab with spaces width
-vim.o.tabstop = 4
-
--- When indenting with '>', use spaces width
-vim.o.shiftwidth = 4
-
--- Update time in ms
-vim.o.updatetime = 100
-
--- Register clipboard
-vim.o.clipboard = "unnamedplus"
-
--- Colorscheme
-vim.cmd("colorscheme sublimemonokai")
-vim.cmd("syntax on")
-vim.o.background = 'dark'
-
--- Leader map
-vim.g.mapleader = ","
-
--- A comma separated list of options for Insert mode completion |ins-completion|
-vim.o.completeopt = "menuone,noinsert,noselect"
-
--- Transparent background
-vim.api.nvim_command("hi Visual guifg=Black guibg=#00FA9A gui=none")
-vim.api.nvim_command("hi Normal guibg=NONE ctermbg=NONE")
-
--- Transparent sign column
-vim.api.nvim_command("hi SignColumn guibg=none")
-
--- Comments color
-vim.api.nvim_command("hi Comment guifg=#7CFC00 guibg=none")
-
--- Number column color
-vim.api.nvim_command("hi LineNr guibg=none guifg=none ctermfg=white")
-
--- Floatin window color
-vim.api.nvim_command("hi NormalFloat guibg=#2F4F4F")
 
 -- Quick mappings
-vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>",        { noremap = true } )
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>",        { noremap = true } )
-vim.api.nvim_set_keymap("n", "<c-d>",     "dd",            { noremap = true } )
-vim.api.nvim_set_keymap("n", "²",         "0",             { noremap = true } )
-vim.api.nvim_set_keymap("n", "<space>",   "i<space><esc>", { noremap = true } )
+api.nvim_set_keymap("n", "<leader>w", ":w<CR>",        { noremap = true } )
+api.nvim_set_keymap("n", "<leader>q", ":q<CR>",        { noremap = true } )
+api.nvim_set_keymap("n", "<c-d>",     "dd",            { noremap = true } )
+api.nvim_set_keymap("n", "²",         "0",             { noremap = true } )
+api.nvim_set_keymap("n", "<space>",   "i<space><esc>", { noremap = true } )
