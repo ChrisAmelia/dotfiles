57430b594d7fae7e3d0f210ba1bdab71fcb0a046
ChrisAmelia <chris@objectdev.io>
2021-01-18 11:30:16 +0100

    chore(LSP signature): change mapping

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -156,7 +156,7 @@ api.nvim_command("hi LspReferenceWrite guibg=" .. COD_GRAY .. " guifg=" .. YELLO
 
 api.nvim_set_keymap("n" , "K"          , "<cmd>lua vim.lsp.buf.hover()<CR>"            , { noremap = true })
 api.nvim_set_keymap("n" , "gi"         , "<cmd>lua vim.lsp.buf.implementation()<CR>"   , { noremap = true })
-api.nvim_set_keymap("n" , "<C-k>"      , "<cmd>lua vim.lsp.buf.signature_help()<CR>"   , { noremap = true })
+api.nvim_set_keymap("i" , "<C-k>"      , "<cmd>lua vim.lsp.buf.signature_help()<CR>"   , { noremap = true })
 api.nvim_set_keymap("n" , "gr"         , "<cmd>lua vim.lsp.buf.references()<CR>"       , { noremap = true })
 api.nvim_set_keymap("n" , "gW"         , "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>" , { noremap = true })
 api.nvim_set_keymap("n" , "gd"         , "<cmd>lua vim.lsp.buf.definition()<CR>"       , { noremap = true })
