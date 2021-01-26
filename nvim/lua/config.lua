86fd86c065ccef3c6792288cd05b4d9744bec67c
ChrisAmelia <chris@objectdev.io>
2021-01-26 09:48:51 +0100

    config(telescope): set defaults

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -104,45 +104,46 @@ api.nvim_command("hi TelescopeNormal guibg=#00000F")
 api.nvim_command("hi TelescopeMatching guifg=#A7EC21")
 
 require('telescope').setup{
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
-    },
-    prompt_position = "bottom",
-    prompt_prefix = ">",
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    layout_defaults = {
-      -- TODO add builtin options.
-    },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    shorten_path = true,
-    winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
-    border = {},
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-    color_devicons = true,
-    use_less = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
-    file_previewer = require'telescope.previewers'.cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
-    grep_previewer = require'telescope.previewers'.vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
-    qflist_previewer = require'telescope.previewers'.qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
+	defaults = {
+		vimgrep_arguments = {
+			'rg',
+			'--color=never',
+			'--no-heading',
+			'--with-filename',
+			'--line-number',
+			'--column',
+			'--smart-case'
+		},
+		prompt_position = "top",
+		prompt_prefix = ">",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_defaults = {
+			-- TODO add builtin options.
+		},
+		file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+		file_ignore_patterns = {},
+		generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+		shorten_path = true,
+		winblend = 0,
+		width = 0.75,
+		preview_cutoff = 120,
+		results_height = 1,
+		results_width = 0.8,
+		border = {},
+		borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+		color_devicons = true,
+		use_less = true,
+		scroll_strategy = 'cycle',
+		set_env = { ['COLORTERM'] = 'truecolor' }, -- default { }, currently unsupported for shells like cmd.exe / powershell.exe
+		file_previewer = require'telescope.previewers'.cat.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_cat.new`
+		grep_previewer = require'telescope.previewers'.vimgrep.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_vimgrep.new`
+		qflist_previewer = require'telescope.previewers'.qflist.new, -- For buffer previewer use `require'telescope.previewers'.vim_buffer_qflist.new`
 
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  }
+		-- Developer configurations: Not meant for general override
+		buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+}
 }
 -- }}}
 
