13d9a1f9328059bc4acd67e80a63175bbc58ceb5
ChrisAmelia <chris@objectdev.io>
2021-09-17 09:33:45 +0200

    fix(nvim-cmp): tab/s-tab to jump placeholder

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -250,6 +250,18 @@ api.nvim_set_keymap("n", "<Leader>bd", ":BufferClose<CR>", { noremap = true, sil
 
 -- nvim-cmp {{{
 
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 local cmp = require('cmp')
 
 cmp.setup {
@@ -312,25 +324,25 @@ cmp.setup {
 			select = true,
 		},
 
-		['<Tab>'] = function(fallback)
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-			elseif vim.fn['vsnip#available'] == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>vsnip-expand-or-jump', true, true, true), '')
+				feedkey("<C-n>", "n")
+			elseif vim.fn["vsnip#available"]() == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
 			else
-				fallback()
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 			end
-		end,
+		end, { "i", "s" }),
 
-		['<S-Tab>'] = function(fallback)
+		["<S-Tab>"] = cmp.mapping(function()
 			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-			elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>vsnip-jump-prev', true, true, true), '')
-			else
-				fallback()
+				feedkey("<C-p>", "n")
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
 			end
-		end,
+		end, { "i", "s" }),
 	},
 
 	sources = {
