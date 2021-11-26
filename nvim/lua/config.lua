c3eb2b9f1d84be400c2e98a0402d1e27a82d7120
ChrisAmelia <chris@objectdev.io>
2021-11-26 09:44:16 +0100

    fix(nvim-cmp): opts -> option

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -355,7 +355,7 @@ cmp.setup {
 		{ name = 'nvim_lsp' },
 		{ name = 'path'     },
 		{ name = 'buffer',
-			opts = {
+			option = {
 				get_bufnrs = function()
 					local bufs = {}
 					for _, win in ipairs(vim.api.nvim_list_wins()) do
