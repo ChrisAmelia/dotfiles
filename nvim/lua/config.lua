163825c253193660a26625b2f50111a038374eec
ChrisAmelia <chris@objectdev.io>
2022-11-08 19:11:28 +0100

    feat(indent_blankline): show context

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -392,6 +392,12 @@ vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {'class', 'function', 'method', '^if', '^while', '^for', '^object', '^table', 'block'}
 vim.g.indent_blankline_char_highlight_list = { 'Function', 'Type', 'String', 'Comment', 'Number' }
 
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
+
 -- }}}
 
 -- pears.nvim {{{
