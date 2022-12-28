3bf5f83d2aea0ef5648550fd28881b8c9249ac2b
ChrisAmelia <chris@objectdev.io>
2022-12-28 21:04:15 +0100

    fix: use vsnip instead of luasnip

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -370,7 +370,7 @@ cmp.setup {
 
 	snippet = {
 		expand = function(args)
-			require'luasnip'.lsp_expand(args.body)
+			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
 
@@ -386,8 +386,8 @@ cmp.setup {
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -398,8 +398,8 @@ cmp.setup {
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
 			else
 				fallback()
 			end
@@ -421,7 +421,7 @@ cmp.setup {
 				end
 			}
 		},
-		{ name = 'luasnip' },
+		{ name = 'vsnip' },
 	},
 }
 
