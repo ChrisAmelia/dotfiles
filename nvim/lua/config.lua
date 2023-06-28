85e9c569d077ce76ccfea99ef0842a558c6b5238
ChrisAmelia <chris@objectdev.io>
2023-06-28 10:33:24 +0200

    fix: add parenthesis on completed

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -486,6 +486,11 @@ require("indent_blankline").setup {
 
 require("nvim-autopairs").setup {}
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
 -- }}}
 
 -- hlargs {{{
