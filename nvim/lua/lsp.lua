a9efc065b2e39464380832a8b24e8dfcb45d6826
ChrisAmelia <chris@objectdev.io>
2025-09-22 11:51:46 +0200

    fix(lua_lus): remove extra conf

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -140,38 +140,6 @@ vim.lsp.config('lua_ls', {
 					return
 				end
 			end
-
-			client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most
-					-- likely LuaJIT in the case of Neovim)
-					version = 'LuaJIT',
-					-- Tell the language server how to find Lua modules same way as Neovim
-					-- (see `:h lua-module-load`)
-					path = {
-						'lua/?.lua',
-						'lua/?/init.lua',
-					},
-				},
-				-- Make the server aware of Neovim runtime files
-				workspace = {
-					checkThirdParty = false,
-					library = {
-						vim.env.VIMRUNTIME
-						-- Depending on the usage, you might want to add additional paths
-						-- here.
-						-- '${3rd}/luv/library'
-						-- '${3rd}/busted/library'
-					}
-					-- Or pull in all of 'runtimepath'.
-					-- NOTE: this is a lot slower and will cause issues when working on
-					-- your own configuration.
-					-- See https://github.com/neovim/nvim-lspconfig/issues/3189
-					-- library = {
-						--   vim.api.nvim_get_runtime_file('', true),
-						-- }
-					}
-				})
 	end,
 
 	settings = {
@@ -189,7 +157,7 @@ vim.lsp.config('lua_ls', {
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.api.nvim_get_runtime_file("", true)
 			},
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
