03567c8123ef79343f3de05f40854df7ee7a0815
ChrisAmelia <vim.incontact@gmail.com>
2026-04-17 19:25:27 +0200

    feat(tree-sitter): add keymap to select inner/outer

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -697,6 +697,25 @@ require("tiny-glimmer").setup({
 
 -- }}}
 
+--- Tree-Sitter incremental node {{{
+-- incremental selection treesitter/lsp
+vim.keymap.set({ "n", "x", "o" }, "<A-o>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_parent(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(vim.v.count1)
+	end
+end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+vim.keymap.set({ "n", "x", "o" }, "<A-i>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_child(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(-vim.v.count1)
+	end
+end, { desc = "Select child treesitter node or inner incremental lsp selections" })
+--- }}}
+
 --- neotest {{{
 
 require("neotest").setup({
